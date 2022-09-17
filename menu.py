17fedd8b385fb0e3346a986af6538e03cef6f775
209 <209@academicos.cesde.edu.co>
2022-09-17 13:12:46 -0500

    Se agregaron condicionales al menu

--- menu.py
+++ menu.py
@@ -3,6 +3,13 @@
 2.Despedir
 3.salir"""
 
-while(){
-  
-}
+while():
+  if(r == 1):
+    print("Hola")
+  elif (r == 2):
+    print("Despedir")
+  elif(r == 3):
+    break
+else:
+    print("Ingrese un numero valido")
+
